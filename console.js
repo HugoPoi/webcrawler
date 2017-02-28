7769b7fe4e82fce086a5fa3901e6cd61db3a0135
HugoPoi <hugo.poissonnet@gmail.com>
2017-02-28 18:41:45 +0100

    Version 2.0, separate lib and cmd tools ! Promise implementation

---
+++ console.js
@@ -0,0 +1,12 @@
+const Webcrawler = require('./lib/crawler');
+const Url = require('url');
+const argv = require('minimist')(process.argv.slice(2));
+const Csv = require('csv-stringify')();
+const fs = require('fs');
+
+let parsedUrl = Url.parse(argv._[0]);
+Webcrawler.crawl({ hostname: parsedUrl.hostname }, [ { url: argv._[0] } ])
+  .then(urls => {
+    Csv.pipe(fs.createWriteStream(parsedUrl.hostname + '_urls.csv'));
+    urls.forEach(url => Csv.write([ url.url, url.statusCode ]) );
+  })
