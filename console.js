6b06d073f1a8aa5d20c84abd1ee8d2cedc45e383
HugoPoi <hugo.poissonnet@gmail.com>
2017-07-28 18:43:46 +0200

    Add option seedFile

--- console.js
+++ console.js
@@ -1,13 +1,26 @@
 const Webcrawler = require('./lib');
 const Url = require('url');
 const argv = require('minimist')(process.argv.slice(2));
-const Csv = require('csv-stringify')();
+const CsvStringify = require('csv-stringify')();
+const CsvParse = require('csv-parse/lib/sync');
 const fs = require('fs');
+const _ = require('lodash');
 
 let parsedUrl = Url.parse(argv._[0]);
+let seedUrls = [ { url: argv._[0] } ];
 
-var csvStream = Csv.pipe(fs.createWriteStream(parsedUrl.hostname + '_urls.csv'));
-let webCrawl = Webcrawler.crawl({ hostname: parsedUrl.hostname, includeSubdomain: argv['include-subdomain'], limit: argv['limit'], concurrency: argv['concurrency'] || 20 }, [ { url: argv._[0] } ]);
+if(argv.seedFile){
+  let parsedSeedFile = CsvParse(fs.readFileSync(argv.seedFile), { columns: ['url', 'statusCode'] })
+  parsedSeedFile.forEach(function filter(urlData){
+    if(urlData.statusCode !== '200'){
+      delete urlData.statusCode;
+    }
+  });
+  seedUrls = parsedSeedFile;
+}
+
+var csvStream = CsvStringify.pipe(fs.createWriteStream(parsedUrl.hostname + '_urls.csv'));
+let webCrawl = Webcrawler.crawl({ hostname: parsedUrl.hostname, includeSubdomain: argv['include-subdomain'], limit: argv['limit'], concurrency: argv['concurrency'] || 20 }, seedUrls);
 
 webCrawl.then(urls => {
   console.log('Crawl %d urls.', urls.length);
@@ -15,5 +28,5 @@ webCrawl.then(urls => {
 });
 
 webCrawl.on('url.done', urlData => {
-  Csv.write([ urlData.url, urlData.statusCode ]);
+  CsvStringify.write([ urlData.url, urlData.statusCode ]);
 });
