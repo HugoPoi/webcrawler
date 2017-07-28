e9700f4716666da92479d042794e4fd45e7777f1
HugoPoi <hugo.poissonnet@gmail.com>
2017-07-28 17:49:38 +0200

    Add concurrency option for console utility

--- console.js
+++ console.js
@@ -5,7 +5,8 @@ const Csv = require('csv-stringify')();
 const fs = require('fs');
 
 let parsedUrl = Url.parse(argv._[0]);
-Webcrawler.crawl({ hostname: parsedUrl.hostname, includeSubdomain: argv['include-subdomain'], limit: argv['limit'] }, [ { url: argv._[0] } ])
+
+Webcrawler.crawl({ hostname: parsedUrl.hostname, includeSubdomain: argv['include-subdomain'], limit: argv['limit'], concurrency: argv['concurrency'] || 20 }, [ { url: argv._[0] } ])
   .then(urls => {
     Csv.pipe(fs.createWriteStream(parsedUrl.hostname + '_urls.csv'));
     urls.forEach(url => Csv.write([ url.url, url.statusCode ]) );
