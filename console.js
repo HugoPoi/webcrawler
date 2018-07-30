f6e5155925816899593ed125a4df13badefc3edc
HugoPoi <hugo@poissonnet.fr>
2018-07-30 15:11:41 +0200

    Breaking change: priorityRegExp is NOW a regexp !, better display management for progress console.js

--- console.js
+++ console.js
@@ -6,6 +6,7 @@ const PromisePipe = require('promisepipe');
 const CsvParse = require('csv-parse/lib/sync');
 const fs = require('fs');
 const _ = require('lodash');
+const moment = require('moment');
 const Gauge = require('gauge');
 
 let parsedUrl = Url.parse(argv._[0]);
@@ -31,6 +32,10 @@ if(argv.seedFile){ // This will rewrite done url in csv
   });
 }
 
+if(argv.priorityRegExp){
+  argv.priorityRegExp = eval(argv.priorityRegExp);
+}
+
 let startTime = new Date();
 
 let webCrawl = new Crawler({
@@ -47,8 +52,39 @@ let webCrawl = new Crawler({
   headers: argv.headers
 }, seedUrls);
 
+
+let gauge;
+if(argv.progress){
+  let lastStats = 0, lastCall = new Date(), count = 0, speed = 0, remainingTime = Infinity;
+  gauge = new Gauge(process.stderr);
+  let compiledProgressMessage = _.template('crawl <%= hostname %> <%= countDone %>/<%= countTotal %> pq:<%= countHighQueue %>  lq:<%= countLowQueue %> s:<%= speed %> rt:<%= remainingTime %>');
+  webCrawl.emitter.on('progress', counts => {
+    if (count++ > (argv.concurrency || 20)) {
+      speed = (counts.done - lastStats) / (new Date() - lastCall) * 1000;
+      remainingTime = counts.todo / speed;
+      count = 0;
+      lastStats = counts.done;
+      lastCall = new Date();
+    }
+    gauge.show(compiledProgressMessage({
+      hostname: parsedUrl.hostname,
+      countDone: counts.done,
+      countTotal: counts.todo + counts.done,
+      countHighQueue: counts.queue,
+      countLowQueue: counts.lowPriorityQueue,
+      speed: speed.toFixed(2),
+      remainingTime: moment.duration(remainingTime, 'seconds').humanize(),
+    }), counts.done / (counts.todo + counts.done));
+  });
+}
+
 webCrawl.promise.then(urls => {
-  console.log('Crawl %d urls. average speed: %d, totalTime: %d', urls.length, urls.length / (new Date() - startTime) * 1000,  (new Date() - startTime) / 1000);
+  if(gauge){
+    gauge.disable();
+  }
+  let averageSpeed = urls.length / (new Date() - startTime) * 1000;
+  let totalTime =  (new Date() - startTime) / 1000;
+  console.log('crawled %d urls. average speed: %d urls/s, totalTime: %ds', urls.length, averageSpeed.toFixed(2), totalTime.toFixed(0));
   if(argv.exportTodoUrls){
     _.filter(urls, url => !url.statusCode).forEach(urlData => writeUrlDataToCsv(urlData));
   }
@@ -60,19 +96,6 @@ webCrawl.emitter.on('url.done', urlData => {
 });
 
 
-if(argv.progress){
-  let lastStats = 0, lastCall = new Date(), count = 0, speed = 0, remainingTime = Infinity;
-  const gauge = new Gauge(process.stderr);
-  webCrawl.emitter.on('progress', counts => {
-    if(count++ > (counts.todo / ( argv.concurrency || 20) ) ){
-      speed = (counts.done - lastStats) / (new Date() - lastCall) * 1000;
-      remainingTime = counts.todo / speed;
-      count = 0;
-      lastStats = counts.done;
-      lastCall = new Date();
-    }
-    gauge.show('crawl ' + parsedUrl.hostname + ' ' + counts.done + '/' + (counts.todo + counts.done) + ' pq:' + counts.queue + ' lq:' + counts.lowPriorityQueue + ' s:' + speed + ' rt:' + remainingTime, counts.done / (counts.todo + counts.done));
-  });
-}
+
 
 process.on('SIGINT', () => webCrawl.stop());
